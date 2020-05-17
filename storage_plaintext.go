cb5bbbe2cf50014a06fec15963b2d2dd10a576c2
Jan Cajthaml <jan.cajthaml@gmail.com>
2020-05-17 21:00:48 +0200

    ensure directory before writing file

--- storage_plaintext.go
+++ storage_plaintext.go
@@ -135,7 +135,7 @@ func (storage PlaintextStorage) WriteFile(path string, data []byte) error {
 // not exist
 func (storage PlaintextStorage) AppendFile(path string, data []byte) error {
 	filename := filepath.Clean(storage.Root + "/" + path)
-	if err := os.MkdirAll((filepath.Base(filename), 0600); err != nil {
+	if err := os.MkdirAll(filepath.Dir(filename), 0600); err != nil {
 		return err
 	}
 	fd, err := syscall.Open(filename, syscall.O_CREAT|syscall.O_WRONLY|syscall.O_APPEND|syscall.O_NONBLOCK, 0600)
